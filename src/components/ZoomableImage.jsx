8d8910f6c99351ad67378fc887c4e0d36ddff9e8
henna <henna.rajakisto@gmail.com>
2025-12-10 12:53:23 +0200

    Zoomableimage siirretty omaan komponenttiin

---
+++ src/components/ZoomableImage.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { Image, Modal } from "@mantine/core";
+
+export function ZoomableImage({ src, alt, imagesize }) {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <>
+      <Image
+        src={src}
+        alt={alt}
+        fit="contain"
+        onClick={() => setOpened(true)}
+        style={{ cursor: "pointer", height: "auto" }}
+        
+      />
+
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+        withCloseButton
+        size="auto"
+        centered
+        overflow="inside"
+        padding={0}
+        styles={{
+          header: { padding: "5px 10px", borderBottom: "1px solid #ddd" },
+          title: { fontSize: "1.1rem", fontWeight: 500, color: "#333" },
+          modal: { maxWidth: "95vw", maxHeight: "95vh" },
+          body: { padding: 0, display: "flex", justifyContent: "center" },
+        }}
+        title={alt}
+      >
+        <Image
+          src={src}
+          alt={alt}
+          fit="contain"
+          style={{ maxHeight: "90vh", width: "100%", height: "auto" }}
+        />
+      </Modal>
+    </>
+  );
+}
