53e80634f856cd5b744c520e87314fa3b5c4953f
henna <henna.rajakisto@gmail.com>
2026-05-06 11:52:27 +0300

    kuva poistettu

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,17 @@
-import { Image } from "@mantine/core";
-
 export default function Contact() {
 
   return (
     <section id="contact" aria-labelledby="contact-title">
-      <div>
-        <Image
-          radius="0"
-          src={"/img/contact-img-sm.png"}
-          alt="decorative image"
-        />
+  
+      <div className="contact-container">
+          <h2>Contact</h2>
+          <p>henna.rajakisto@gmail.com</p>
+          <div className="contact-icons-flex">
+          <a href="https://github.com/hennamarleena"><img src="/img/github-icon.png" alt="github icon" className="icon" /></a>
+          <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/"><img src="/img/linkedin-square-icon.png" alt="linkedin icon" className="icon" /></a></div>
+        {/* </div> */}
       </div>
-
-      <div className="contact-text">
-        <h2>Contact</h2>
-        <h3>Email</h3>
-        <p>henna.rajakisto@gmail.com</p>
-        <div className="contact-icons-flex">
-        <a href="https://github.com/hennamarleena"><img src="/img/github-icon.png" alt="github icon" className="icon" /></a>
-        <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/"><img src="/img/linkedin-square-icon.png" alt="linkedin icon" className="icon" /></a>
-        </div>
-      </div>
+      
     </section>
   );
 }
