c69b91390ce16b9e0af3c11bef55b0f30838ecf3
henna <henna.rajakisto@gmail.com>
2026-05-18 12:39:34 +0300

    contact linkit cardin sisään

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,60 @@
 export default function Contact() {
-
   return (
     <section id="contact" aria-labelledby="contact-title">
-  
-      <div className="contact-container">
-          <h2>Contact</h2>
-          <p>henna.rajakisto@gmail.com</p>
-          <div className="contact-icons-flex">
-          <a href="https://github.com/hennamarleena"><img src="/img/github-icon.png" alt="github icon" className="icon" /></a>
-          <a href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/"><img src="/img/linkedin-square-icon.png" alt="linkedin icon" className="icon" /></a></div>
-        {/* </div> */}
+      <div className="contact-card">
+        <h2>Let's Connect</h2>
+
+        <p>
+          Interested in working together or just want to say hi?
+        </p>
+        <div className="contact-links">
+
+          
+            <a
+              className="contact-row"
+              href="mailto:henna.rajakisto@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div>
+                <img src="/img/email-icon.webp" alt="GitHub" />
+                <div>
+                  <h4>Email</h4>
+                  <span>henna.rajakisto@gmail.com</span>
+
+                </div>
+              </div>
+            </a>
+          
+          <a
+            className="contact-row"
+            href="https://github.com/hennamarleena"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <img src="/img/github-icon.png" alt="GitHub" />
+              <div>
+                <h4>GitHub</h4>
+              </div>
+            </div>
+          </a>
+
+          <a
+            className="contact-row"
+            href="https://www.linkedin.com/in/henna-rajakisto-b57b99253/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div>
+              <img src="/img/linkedin-square-icon.png" alt="LinkedIn" />
+              <div>
+                <h4>LinkedIn</h4>
+              </div>
+            </div>
+          </a>
+        </div>
       </div>
-      
     </section>
   );
 }
